2df988055dd08a0e8268fc84e6fccce1bb26b79f
Darius Marian <marian.darius98@gmail.com>
2020-03-22 18:22:05 +0200

    A blazing fast implementation of StacktraceSymbolizer for MacOS.

--- executable/stacktrace_symbolizer_macos.cpp
+++ executable/stacktrace_symbolizer_macos.cpp
@@ -1,44 +1,141 @@
 #include "stacktrace_symbolizer.hpp"
 
 #include <iomanip>
+#include <iostream>
 #include <sstream>
+
+#include <signal.h>
+#include <unistd.h>
+
+#define PIPE_READ 0
+#define PIPE_WRITE 1
 
 namespace syan {
 
-class StacktraceSymbolizerImpl: public StacktraceSymbolizer {
+class StacktraceSymbolizerImpl final: public StacktraceSymbolizer {
 public:
   StacktraceSymbolizerImpl(const std::string& binary_file_path,
                            const DumpFileHeader& dump_file_header)
-      : binary_file_path(binary_file_path),
-        dump_file_header(dump_file_header) {}
+      : binary_file_path(binary_file_path), dump_file_header(dump_file_header) {
+    int stdin_pipe[2];
+    if (pipe(stdin_pipe) < 0) {
+      std::cerr << "Failed to open stdin pipe to stacktrace symbolizer process"
+                << " errno=" << errno << " (" << strerror(errno) << ")\n";
+      std::abort();
+    }
+
+    int stdout_pipe[2];
+    if (pipe(stdout_pipe) < 0) {
+      std::cerr << "Failed to open stdout pipe to stacktrace symbolizer process"
+                << " errno=" << errno << " (" << strerror(errno) << ")\n";
+      std::abort();
+    }
+
+    atos_pid = fork();
+    if (atos_pid < 0) {
+      std::cerr << "Failed to fork stacktrace symbolizer process"
+                << " errno=" << errno << " (" << strerror(errno) << ")\n";
+      std::abort();
+    }
+    if (atos_pid > 0) {  // parent is done.
+      atos_stdin = stdin_pipe[PIPE_WRITE];
+      atos_stdout = stdout_pipe[PIPE_READ];
+      close(stdin_pipe[PIPE_READ]);
+      close(stdout_pipe[PIPE_WRITE]);
+      return;
+    }
+
+    if (dup2(stdin_pipe[PIPE_READ], STDIN_FILENO) < 0) {
+      std::cerr << "Failed to pipe stacktrace symbolizer process's stdin"
+                << " errno=" << errno << " (" << strerror(errno) << ")\n";
+      std::abort();
+    }
+
+    if (dup2(stdout_pipe[PIPE_WRITE], STDOUT_FILENO) < 0) {
+      std::cerr << "Failed to pipe stacktrace symbolizer process's stdout"
+                << " errno=" << errno << " (" << strerror(errno) << ")\n";
+      std::abort();
+    }
+
+    close(stdin_pipe[PIPE_READ]);
+    close(stdin_pipe[PIPE_WRITE]);
+    close(stdout_pipe[PIPE_READ]);
+    close(stdout_pipe[PIPE_WRITE]);
+
+    std::stringstream load_address_builder;
+    load_address_builder << std::hex << std::setfill('0') << std::setw(16)
+                         << dump_file_header.program_load_addr + 8;
+    std::string load_address = load_address_builder.str();
+    // TODO: Don't hardcode architecture here!
+    const char* args[] = {"/usr/bin/atos",
+                          "-arch",
+                          "x86_64",
+                          "-o",
+                          binary_file_path.c_str(),
+                          "-l",
+                          load_address.c_str(),
+                          nullptr};
+    if (execv("/usr/bin/atos", (char* const*)args) != 0) {
+      std::cerr << "Failed to execute stacktrace symbolizer process"
+                << " errno=" << errno << " (" << strerror(errno) << ")\n";
+      std::abort();
+    }
+  }
+
+  ~StacktraceSymbolizerImpl() final {
+    kill(atos_pid, SIGKILL);
+    close(atos_stdin);
+    close(atos_stdout);
+  }
 
   void symbolize_stacktrace(const Event& event, std::ostream& out) final {
     std::stringstream atos_command_builder;
-    // TODO: Don't hardcode architecture here!
-    atos_command_builder << "atos -o " << binary_file_path
-                         << " -arch x86_64 -l 0x" << std::hex
-                         << std::setfill('0') << std::setw(16)
-                         << dump_file_header.program_load_addr + 8;
     for (const auto& pc : event.raw_backtrace()) {
       if (pc != 0) {
-        atos_command_builder << " 0x" << std::hex << std::setfill('0')
-                             << std::setw(16) << pc;
+        atos_command_builder << "0x" << std::hex << std::setfill('0')
+                             << std::setw(16) << pc << " ";
       }
     }
+    atos_command_builder << '\n';
+    std::string atos_command = atos_command_builder.str();
+    size_t total_written = 0;
+    while (total_written < atos_command.size()) {
+      ssize_t written = write(atos_stdin, atos_command.data() + total_written,
+                              atos_command.size() - total_written);
+      if (written < 0) {
+        std::cerr << "Failed to write input to stacktrace symbolizer"
+                  << " errno=" << errno << " (" << strerror(errno) << ")\n";
+        std::abort();
+      }
+      total_written += written;
+    }
+
+    int num_lines_expected =
+        std::count_if(std::begin(event.raw_backtrace()),
+                      std::end(event.raw_backtrace()), [](auto pc) {
+                        return pc != 0;
+                      });
+
+    int num_lines = 0;
     out << "\t\t";
-    auto atos_command = atos_command_builder.str();
-    std::FILE* atos_process = popen(atos_command.c_str(), "r");
     char ch;
-    while ((ch = std::fgetc(atos_process)) != EOF) {
+    while (true) {
+      read(atos_stdout, &ch, 1);
       out << ch;
       if (ch == '\n') {
+        num_lines += 1;
+        if (num_lines == num_lines_expected) {
+          break;
+        }
         out << "\t\t";
       }
     }
-    pclose(atos_process);
   }
 
 private:
+  int atos_pid;
+  int atos_stdin;
+  int atos_stdout;
   std::string binary_file_path;
   DumpFileHeader dump_file_header;
 };
